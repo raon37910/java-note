e629e18ea6199d5e21681567287b1d90d8671d92
김정민 <raon37910@gmail.com>
2025-01-09 07:57:58 +0900

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("java")
+}
+
+group = "com.raon"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+// 멀티 모듈 프로젝트 전체에 범용적으로 적용할 수 있는 설정
+subprojects {
+    apply(plugin = "java")
+
+    group = "com.raon"
+    version = "1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        testImplementation(platform("org.junit:junit-bom:5.10.0"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
+}
