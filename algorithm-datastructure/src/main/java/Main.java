477d18e1b627122203b4896e27fde4a39c6152a2
김정민 <raon37910@gmail.com>
2025-01-10 20:46:10 +0900

    1월 10일 알고리즘 풀이 종료

--- algorithm-datastructure/src/main/java/Main.java
+++ algorithm-datastructure/src/main/java/Main.java
@@ -8,14 +8,65 @@ public class Main {
 	public static StringTokenizer st;
 	public static int[][] arr;
 	public static int n;
+	public static int m;
+	public static int result = 0;
 
+	// 행복한 수열은 연속하여 m개 이상의 동일한 원소가 나오는 순간이 존재하는 수열이다.
+	// 행마다 나오는 n개의 수열과 열마다 나오는 n개의 수열 총 2n 개의 수열이 존재한다.
+	// 행복한 수열의 개수를 세시오.
 	public static void main(String[] args) throws Exception {
 		input();
 
+		for(int i = 1; i <= n; i++) {
+			if(countRow(i) >= m ) {
+				result++;
+			}
+
+			if(countCol(i) >= m ) {
+				result++;
+			}
+		}
+		
+		System.out.println(result);
+	}
+
+	// row번째 행에서 가장긴 연속으로 나오는 숫자의 개수를 센다.
+	public static int countRow(int row) {
+		int max = 1;
+		int cur = 1;
+
+		for(int i = 2; i <= n; i++) {
+			if(arr[row][i] == arr[row][i-1]) {
+				cur++;
+			} else {
+				max = Math.max(max, cur);
+				cur = 1;
+			}
+		}
+
+		return Math.max(max, cur);
+	}
+
+	public static int countCol(int col) {
+		int max = 1;
+		int cur = 1;
+
+		for(int i = 2; i <= n; i++) {
+			if(arr[i][col] == arr[i-1][col]) {
+				cur++;
+			} else {
+				max = Math.max(max, cur);
+				cur = 1;
+			}
+		}
+
+		return Math.max(max, cur);
 	}
 
 	public static void input() throws Exception {
-		n = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 		arr = new int[n+1][n+1];
 
 		for(int i = 1; i <= n; i++) {
@@ -26,6 +77,4 @@ public class Main {
 			}
 		}
 	}
-
-
 }
